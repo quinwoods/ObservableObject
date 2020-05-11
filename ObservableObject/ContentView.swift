95907f202d104231671d06a818d9b8d6aeb1d6a9
quinwoods <quinwoodz@gmail.com>
2020-05-11 12:21:22 -0400

    Initial Commit

---
+++ ObservableObject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ObservableObject
+//
+//  Created by Quin’darius Lyles-Woods on 5/11/20.
+//  Copyright © 2020 Quin'darius. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
